4e7fe4ab217c23fa25c34ec4bb5d33ac237fca69
yangxk15 <yangxk15@gmail.com>
2017-02-27 16:09:51 -0500

    Populate PantryFragment using arrayadapter and items.

--- app/src/main/java/edu/dartmouth/cs/pantryplanner/pantryplanner/model/Item.java
+++ app/src/main/java/edu/dartmouth/cs/pantryplanner/pantryplanner/model/Item.java
@@ -3,11 +3,13 @@ package edu.dartmouth.cs.pantryplanner.pantryplanner.model;
 import java.util.Calendar;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 /**
  * Created by yangxk15 on 2/27/17.
  */
 
+@Data
 public class Item {
     String id;
     String name;
@@ -19,5 +21,10 @@ public class Item {
         this.itemType = itemType;
         this.timestamp = Calendar.getInstance();
         this.id = name + timestamp.getTimeInMillis();
+    }
+
+    public int getCountdownDays() {
+        long diff = Calendar.getInstance().getTimeInMillis() - timestamp.getTimeInMillis();
+        return (int) (diff / (1000 * 60 * 60 * 24));
     }
 }
