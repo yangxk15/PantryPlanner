6d4ab3be8b7046bf933d0a6141939318b2a07a41
yangxk15 <yangxk15@gmail.com>
2017-03-03 15:41:51 -0500

    Change enum to string in backend models

--- app/src/main/java/edu/dartmouth/cs/pantryplanner/app/controller/CreateRecipeFragment.java
+++ app/src/main/java/edu/dartmouth/cs/pantryplanner/app/controller/CreateRecipeFragment.java
@@ -17,7 +17,7 @@ public class CreateRecipeFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_create_recipe, container, false);
+        return inflater.inflate(R.layout.activity_create_recipe, container, false);
     }
 
 }
